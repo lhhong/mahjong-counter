13b633f5dde02285caf192e1e15d4a3b8dd758bd
Heok Hong Low <heok.hong.low@gmail.com>
2020-04-13 21:21:41 +0800

    Remove bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { GameSetupView } from "./components/GameSetupView";
 import { GainsView } from "./components/GainsView";
 import { ActionView } from "./components/ActionView";
